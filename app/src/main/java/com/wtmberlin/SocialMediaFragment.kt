9d3f141e06291d812870ae2adb82df1f874a46bc
Anna Morgiel <amorgiel@gmail.com>
2019-01-02 19:35:36 +0100

    bugfix social media

--- app/src/main/java/com/wtmberlin/SocialMediaFragment.kt
+++ app/src/main/java/com/wtmberlin/SocialMediaFragment.kt
@@ -9,6 +9,6 @@ import androidx.fragment.app.Fragment
 class SocialMediaFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_detail_social_media, container)
+        return inflater.inflate(R.layout.fragment_detail_social_media, container, false)
     }
 }
