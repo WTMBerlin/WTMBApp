547e4d6b234a1195b2d6982f396b724c5bd57745
Anna Morgiel <amorgiel@gmail.com>
2019-01-09 16:22:52 +0100

    Clean up strings.xml
    From now on strings.xml are organized into sections for each screen and their names are prefixed with the screen name

--- app/src/main/java/com/wtmberlin/ui/CommunityLocalFragment.kt
+++ app/src/main/java/com/wtmberlin/ui/CommunityLocalFragment.kt
@@ -7,12 +7,12 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.squareup.picasso.Picasso
 import com.wtmberlin.R
-import kotlinx.android.synthetic.main.community_info_local_screen.*
+import kotlinx.android.synthetic.main.community_local_screen.*
 
-class CommunityInfoFragment : Fragment() {
+class CommunityLocalFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(
-            R.layout.community_info_local_screen,
+            R.layout.community_local_screen,
             container,
             false
         )
