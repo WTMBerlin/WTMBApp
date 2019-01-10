6fa1abed9bd8eee9cdeb40d72a8b7267562e74ad
Marcin Jedynak <m.jedyny@gmail.com>
2019-01-10 14:30:45 +0100

    Migrate Gradle build scripts to Kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,14 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.11'
-    ext.lifecycle_version = '2.0.0'
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.2.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath "android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-alpha09"
+        classpath("com.android.tools.build:gradle:${Deps.gradleVersion}")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:${Deps.kotlinVersion}")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:${Deps.navigationVersion}")
 
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -24,6 +22,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register<Delete>("clean").configure {
+    delete(rootProject.buildDir)
 }
