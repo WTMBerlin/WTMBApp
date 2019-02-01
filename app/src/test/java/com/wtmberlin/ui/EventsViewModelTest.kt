f72de701b9aecdb7a37198711b0bff31f069d676
Anna Morgiel <amorgiel@gmail.com>
2019-02-02 00:56:42 +0100

    add test draft

--- app/src/test/java/com/wtmberlin/ui/EventsViewModelTest.kt
+++ app/src/test/java/com/wtmberlin/ui/EventsViewModelTest.kt
@@ -12,6 +12,7 @@ import io.reactivex.processors.PublishProcessor
 import org.junit.Rule
 import org.junit.Test
 import org.mockito.Mockito.`when`
+import org.threeten.bp.ZonedDateTime
 
 class EventsViewModelTest {
     @get:Rule
@@ -32,5 +33,29 @@ class EventsViewModelTest {
 
         assertThat(refreshObserver.observedValues)
             .containsExactly(true)
+    }
+
+    @Test
+    fun `when events are refreshed emits refresh false`() {
+        val refreshObserver = viewModel.refreshing.testObserver()
+
+        mockEvents.onNext(Result(loading = false, data = null, error = null))
+
+        assertThat(refreshObserver.observedValues)
+            .containsExactly(false)
+    }
+
+    @Test
+    fun `when data emits events`() {
+        val refreshObserver = viewModel.refreshing.testObserver()
+        val dataOld: MutableList<WtmEvent> = mutableListOf<WtmEvent>()
+        dataOld.add(0,element = WtmEvent("1","Android Co-Learning Testing part 1", TODO(),TODO(),"amazing event","fake url",TODO()))
+        dataOld.add(1,element = WtmEvent("2","Android Co-Learning Testing part 2", TODO(),TODO(),"amazing event","fake url",TODO()))
+        dataOld.add(2,element = WtmEvent("3","Android Co-Learning Testing part 3", TODO(),TODO(),"amazing event","fake url",TODO()))
+        dataOld.add(3,element = WtmEvent("4","Android Co-Learning Testing part 4", TODO(),TODO(),"amazing event","fake url",TODO()))
+
+        mockEvents.onNext(Result(loading = false, data = dataOld, error = null))
+        assertThat(TODO())
+            .containsExactly(TODO())
     }
 }
