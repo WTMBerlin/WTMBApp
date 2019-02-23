46b08c5c9a5befdef605055ab9843116ab8c260d
Marcin Jedynak <m.jedyny@gmail.com>
2019-02-23 13:58:08 +0100

    Fix link to WTM website

--- app/src/main/java/com/wtmberlin/ui/MainActivity.kt
+++ app/src/main/java/com/wtmberlin/ui/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.wtmberlin.ui
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -20,12 +18,6 @@ class MainActivity : AppCompatActivity() {
 
         toolbar.setupWithNavController(navController, appBarConfiguration)
         nav_view.setupWithNavController(findNavController(R.id.nav_host_fragment))
-    }
-
-    private fun displayGlobalCommunityInfo() {
-        val openURL = Intent(android.content.Intent.ACTION_VIEW)
-        openURL.data = Uri.parse("https://www.womentechmakers.com")
-        startActivity(openURL)
     }
 }
 
