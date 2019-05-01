301ddd607f279223b9dd131d94a93ca16e47f962
Anna Morgiel <18116883+annamorgiel@users.noreply.github.com>
2019-05-01 13:43:40 +0200

    add draft of the navigation to the reviews screen

--- app/src/main/java/com/wtmberlin/ui/CommunityLocalFragment.kt
+++ app/src/main/java/com/wtmberlin/ui/CommunityLocalFragment.kt
@@ -1,10 +1,13 @@
 package com.wtmberlin.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavDeepLinkBuilder
 import com.squareup.picasso.Picasso
 import com.wtmberlin.R
 import kotlinx.android.synthetic.main.community_local_screen.*
@@ -22,5 +25,14 @@ class CommunityLocalFragment : Fragment() {
         Picasso.get()
             .load("https://raw.githubusercontent.com/WTMBerlin/slides/master/logos/fb-cover.jpg")
             .into(logo)
+
+        view.setOnClickListener { openReviews(view) }
+    }
+
+    fun openReviews(view:View){
+        NavDeepLinkBuilder(view.context)
+            .setGraph(R.navigation.nav_graph)
+            .setDestination(R.id.reviews_screen)
+            .createPendingIntent()
     }
 }
