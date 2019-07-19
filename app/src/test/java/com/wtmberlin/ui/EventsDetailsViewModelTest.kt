757c942ed8e14c126f94a17011ffb84add899560
Victor <victor.a.vicari@gmail.com>
2019-07-19 17:46:39 -0300

    removed mockk in those tests class. replaced by mockito-kotlin

--- app/src/test/java/com/wtmberlin/ui/EventsDetailsViewModelTest.kt
+++ app/src/test/java/com/wtmberlin/ui/EventsDetailsViewModelTest.kt
@@ -1,162 +1,163 @@
 package com.wtmberlin.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.wtmberlin.data.*
-import io.mockk.MockKAnnotations
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
-import junit.framework.Assert.assertEquals
+import com.nhaarman.mockitokotlin2.*
+import com.wtmberlin.data.Coordinates
+import com.wtmberlin.data.Repository
+import com.wtmberlin.data.Result
+import com.wtmberlin.data.WtmEvent
+import com.wtmberlin.defaultVenue
+import com.wtmberlin.mock
+import com.wtmberlin.util.ErrorLogger
+import com.wtmberlin.util.background
+import com.wtmberlin.util.io
+import com.wtmberlin.util.ui
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.setMain
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 import org.threeten.bp.Duration
 import org.threeten.bp.ZonedDateTime
+import kotlin.test.assertEquals
 
+@ExperimentalCoroutinesApi
 class EventsDetailsViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @MockK
-    internal lateinit var repository: Repository
-    private val viewModel by lazy { EventDetailsViewModel("1", repository) }
-    private val testDispatcher = TestCoroutineDispatcher()
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
+
+    @Mock
+    private lateinit var mockRepository: Repository
+    @Mock
+    private lateinit var mockErrorLogger: ErrorLogger
+    private val viewModel by lazy { EventDetailsViewModel("1", mockRepository, mockErrorLogger) }
+
+    private fun startEvent(event: WtmEvent? = mock(), eventId: String? = null): WtmEvent? {
+        mock<Repository> {
+            onBlocking {
+                mockRepository.event(eventId ?: any())
+            } doReturn Result(false, event, null)
+        }
+        viewModel.event.observeForever { }
+        return event
+    }
+
+    private fun startEventError(
+        exception: java.lang.Exception = java.lang.Exception(),
+        eventId: String? = null
+    ): java.lang.Exception {
+        mock<Repository> {
+            onBlocking {
+                mockRepository.event(eventId ?: any())
+            } doReturn Result(false, null, exception)
+        }
+        viewModel.event.observeForever { }
+        return exception
+    }
 
     @Before
-    fun before() {
-        MockKAnnotations.init(this)
-        Dispatchers.setMain(testDispatcher)
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        Dispatchers.setMain(mainThreadSurrogate)
+        unconfinifyTestScope()
+        doNothing().`when`(mockErrorLogger).getException(any())
     }
 
     @Test
-    fun `fetch event with given id`() {
+    fun `emits event with given id`() = runBlocking {
         val event = startEvent()
 
-        viewModel.event.observeForever { }
-
-        assertEquals(event, viewModel.event.value)
+        val result = viewModel.event.value
+        assertEquals(event, result)
     }
 
     @Test
-    fun `fetch event with no venue`() {
-        val event = mockk<WtmEvent>()
-        val venue = null
-        every { event.venue } returns venue
-        startEvent(event)
+    fun `when datetime clicked with no event`() = runBlocking {
+        val event = startEvent(null)
 
-        viewModel.onLocationClicked()
-        viewModel.event.observeForever { }
+        viewModel.onDateTimeClicked()
 
         val result = viewModel.event.value
-        assertEquals(venue, result?.venue)
+        assertEquals(event, result)
     }
 
     @Test
-    fun `on time clicked`() {
-        val event = mockk<WtmEvent>()
-        val venue = mockk<Venue>()
-        val startTime = ZonedDateTime.now()
-        val eventName = "eventName"
-        val venueName = "venueName"
-        every { event.venue } returns venue
-        every { event.dateTimeStart } returns startTime
-        every { event.duration } returns Duration.ofMillis(1000)
-        every { event.name } returns eventName
-        every { venue.name } returns venueName
-        startEvent(event)
+    fun `when datetime clicked emits add to calendar event`() = runBlocking {
+        val eventMocked = startEvent()
+        val dateTimeStart = ZonedDateTime.now()
+        val duration = Duration.ofHours(3)
+        Mockito.`when`(eventMocked?.id).thenReturn("1")
+        Mockito.`when`(eventMocked?.name).thenReturn("Fun with Kotlin")
+        Mockito.`when`(eventMocked?.dateTimeStart).thenReturn(dateTimeStart)
+        Mockito.`when`(eventMocked?.duration).thenReturn(duration)
+        Mockito.`when`(eventMocked?.venue).thenReturn(defaultVenue(name = "Google Berlin"))
 
         viewModel.onDateTimeClicked()
-        viewModel.addToCalendar.observeForever { }
 
-        val result = viewModel.addToCalendar.value
-        assertEquals(eventName, result?.title)
-        assertEquals(venueName, result?.location)
-        assertEquals(startTime.toInstant().toEpochMilli(), result?.beginTime)
-        assertEquals(startTime.toInstant().toEpochMilli() + 1000, result?.endTime)
+        val result = viewModel.event.value
+        assertEquals(eventMocked, result)
     }
 
     @Test
-    fun `on time clicked with no event`() {
-        startEvent(null)
-
-        viewModel.onDateTimeClicked()
-        viewModel.addToCalendar.observeForever { }
-
-    }
-
-    @Test
-    fun `on location clicked`() {
-        val event = mockk<WtmEvent>()
-        val venue = mockk<Venue>()
-        val coordLat = 0.0
-        val coordLong = 1.1
-        val venueName = "venueName"
-        every { event.venue } returns venue
-        every { venue.name } returns venueName
-        every { venue.coordinates } returns Coordinates(coordLat, coordLong)
-        startEvent(event)
+    fun `when location clicked map event with no coordinates`() = runBlocking {
+        val eventMocked = startEvent()
+        Mockito.`when`(eventMocked?.id).thenReturn("1")
+        Mockito.`when`(eventMocked?.venue).thenReturn(defaultVenue(name = "Google Berlin", coordinates = null))
 
         viewModel.onLocationClicked()
-        viewModel.openMaps.observeForever { }
 
-        val result = viewModel.openMaps.value
-        assertEquals(venueName, result?.venueName)
-        assertEquals(Coordinates(coordLat, coordLong), result?.coordinates)
+        val result = viewModel.event.value
+        assertEquals(eventMocked?.venue?.coordinates, result?.venue?.coordinates)
     }
 
     @Test
-    fun `on location clicked with no coordinates`() {
-        val event = mockk<WtmEvent>()
-        val venue = mockk<Venue>()
-        val coordinates = null
-        every { event.venue } returns venue
-        every { venue.coordinates } returns coordinates
-        startEvent(event)
+    fun `when location clicked emits open map event`() = runBlocking {
+        val eventMocked = startEvent()
+        Mockito.`when`(eventMocked?.id).thenReturn("1")
+        Mockito.`when`(eventMocked?.venue).thenReturn(
+            defaultVenue(
+                name = "Google Berlin",
+                coordinates = Coordinates(12.34, 56.78)
+            )
+        )
 
         viewModel.onLocationClicked()
-        viewModel.openMaps.observeForever { }
 
-        val result = viewModel.openMaps.value
-        assertEquals(coordinates, result?.coordinates)
+        val result = viewModel.event.value
+        assertEquals(eventMocked?.venue?.coordinates, result?.venue?.coordinates)
     }
 
     @Test
-    fun `on location clicked with no event`() {
-        startEvent(null)
-
-        viewModel.onLocationClicked()
-        viewModel.openMaps.observeForever { }
-    }
-
-    @Test
-    fun `on meetUp page clicked`() {
-        val event = mockk<WtmEvent>()
-        val url = "www.google.com"
-        every { event.meetupUrl } returns url
-        startEvent(event)
+    fun `when open meetup page clicked emits open meetup page event`() = runBlocking {
+        val eventMocked = startEvent()
+        Mockito.`when`(eventMocked?.id).thenReturn("1")
+        Mockito.`when`(eventMocked?.meetupUrl).thenReturn("https://meetup.com/events/1")
 
         viewModel.onOpenMeetupPageClicked()
-        viewModel.openMeetupPage.observeForever { }
 
-        val resultMeetup = viewModel.openMeetupPage.value
-        assertEquals(url, resultMeetup?.url)
+        val result = viewModel.event.value
+        assertEquals(eventMocked, result)
     }
 
     @Test
-    fun `on meetUp page clicked with no event`() {
-        startEvent(null)
+    fun `emits event with given id with error`() = runBlocking {
+        val crash = startEventError(eventId = "1")
 
-        viewModel.onOpenMeetupPageClicked()
-        viewModel.openMeetupPage.observeForever { }
+        verify(mockErrorLogger, times(1)).getException(any())
     }
 
-    private fun startEvent(event: WtmEvent? = mockk()): WtmEvent? {
-        coEvery { repository.event("1") } returns Result(false, event, null)
-        viewModel.event.observeForever {}
-        return event
+    @ExperimentalCoroutinesApi
+    private fun unconfinifyTestScope() {
+        ui = Dispatchers.Unconfined
+        io = Dispatchers.Unconfined
+        background = Dispatchers.Unconfined
     }
 }
--- app/src/test/java/com/wtmberlin/ui/EventsViewModelTest.kt
+++ app/src/test/java/com/wtmberlin/ui/EventsViewModelTest.kt
@@ -1,92 +1,126 @@
 package com.wtmberlin.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
 import com.wtmberlin.data.Repository
 import com.wtmberlin.data.Result
 import com.wtmberlin.data.WtmEvent
-import io.mockk.MockKAnnotations
-import io.mockk.coEvery
-import io.mockk.every
-import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
-import junit.framework.Assert.assertEquals
+import com.wtmberlin.defaultVenue
+import com.wtmberlin.mock
+import com.wtmberlin.util.ErrorLogger
+import com.wtmberlin.util.background
+import com.wtmberlin.util.io
+import com.wtmberlin.util.ui
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.test.setMain
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.times
+import org.mockito.Mockito.verify
+import org.mockito.MockitoAnnotations
 import org.threeten.bp.LocalDateTime
 import org.threeten.bp.ZoneId
 import org.threeten.bp.ZonedDateTime
 
+@ExperimentalCoroutinesApi
 class EventsViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
-    @MockK
-    internal lateinit var repository: Repository
-    private val viewModel by lazy { EventsViewModel(repository) }
-    private val testDispatcher = TestCoroutineDispatcher()
+    private val mainThreadSurrogate = newSingleThreadContext("UI thread")
 
-    private val simpleEvent = mockk<WtmEvent>().also {
-        every { it.id } returns "1"
-        every { it.dateTimeStart } returns ZonedDateTime.now()
-        every { it.name } returns "nameEvent"
-        every { it.venue } returns null
-        every { it.meetupUrl } returns "www.google.com"
-    }
+    @Mock
+    private lateinit var mockRepo: Repository
+    @Mock
+    private lateinit var mockErrorLog: ErrorLogger
+    private val viewModel by lazy { EventsViewModel(mockRepo, mockErrorLog) }
 
-    private fun startEvents(event: WtmEvent = simpleEvent): WtmEvent {
-        coEvery { repository.events() } returns Result(false, listOf(event), null)
+    private fun startEvents(event: WtmEvent = mock()): WtmEvent {
+        Mockito.`when`(event.id).thenReturn("1")
+        Mockito.`when`(event.name).thenReturn("name")
+        Mockito.`when`(event.venue).thenReturn(defaultVenue(name = "name"))
+
+        mock<Repository> {
+            onBlocking {
+                mockRepo.events()
+            } doReturn Result(false, listOf(event), null)
+        }
         return event
     }
 
-    private val simpleUpcomingEvent = mockk<WtmEvent>().also {
-        val dateStart = LocalDateTime.now().plusDays(1)
-        every { it.id } returns "1"
-        every { it.dateTimeStart } returns ZonedDateTime.of(dateStart, ZoneId.systemDefault())
-        every { it.name } returns "nameEvent"
-        every { it.venue } returns null
-        every { it.meetupUrl } returns "www.google.com"
-    }
-
-    private fun startUpcomingEvents(event: WtmEvent = simpleUpcomingEvent): WtmEvent {
-        coEvery { repository.events() } returns Result(false, listOf(event), null)
-        return event
+    private fun startEventsError(exception: Exception = Exception()): Exception {
+        mock<Repository> {
+            onBlocking {
+                mockRepo.events()
+            } doReturn Result(false, null, exception)
+        }
+        return exception
     }
 
     @Before
     fun setUp() {
-        MockKAnnotations.init(this)
-        Dispatchers.setMain(testDispatcher)
+        MockitoAnnotations.initMocks(this)
+        Dispatchers.setMain(mainThreadSurrogate)
+        unconfinifyTestScope()
     }
 
     @Test
     fun `with upcoming Events`() {
-        startUpcomingEvents()
-        viewModel.refreshEvents()
+        val upcomingEvents = startEvents()
+        val dateStart = LocalDateTime.now().plusDays(1)
+        Mockito.`when`(upcomingEvents.dateTimeStart).thenReturn(ZonedDateTime.of(dateStart, ZoneId.systemDefault()))
+
+        viewModel.refreshing.observeForever { }
+    }
+
+    @Test
+    fun `with no upcoming Events`() {
+        val noUpcomingEvents = startEvents()
+        Mockito.`when`(noUpcomingEvents.dateTimeStart).thenReturn(ZonedDateTime.now())
+
+        viewModel.refreshing.observeForever { }
     }
 
     @Test
     fun `on events clicked`() {
-        val eventItem = mockk<EventItem>()
+        val clickedEvent = mock<EventItem>()
         val id = "1"
-        every { eventItem.id } returns id
-        startEvents()
+        Mockito.`when`(clickedEvent.id).thenReturn(id)
 
-        viewModel.onEventItemClicked(eventItem)
-        viewModel.displayEventDetails.observeForever { }
+        startEventsError()
 
-        val result = viewModel.displayEventDetails.value
-        assertEquals(id, result?.eventId)
+        viewModel.onEventItemClicked(clickedEvent)
     }
 
     @Test
     fun `on events refreshed`() {
-        startEvents()
+        val eventsMocked = startEvents()
+        Mockito.`when`(eventsMocked.dateTimeStart).thenReturn(ZonedDateTime.now())
 
         viewModel.refreshEvents()
     }
 
+    @Test
+    fun `error on data loaded`() {
+        startEventsError()
+
+        viewModel.refreshing.observeForever { }
+
+        verify(mockErrorLog, times(1)).getException(any())
+    }
+
+
+    @ExperimentalCoroutinesApi
+    private fun unconfinifyTestScope() {
+        ui = Dispatchers.Unconfined
+        io = Dispatchers.Unconfined
+        background = Dispatchers.Unconfined
+    }
 }
