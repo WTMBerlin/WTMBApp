a3bde5a55b0609c808cc0749c723bb30feaa9e4c
Anna Morgiel <18116883+annamorgiel@users.noreply.github.com>
2020-01-26 22:01:02 +0100

    remove rotation toy accessibility

--- app/src/main/java/com/wtmberlin/ui/AccessibilityFragment.kt
+++ app/src/main/java/com/wtmberlin/ui/AccessibilityFragment.kt
@@ -6,27 +6,18 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.wtmberlin.R
-import kotlinx.android.synthetic.main.accessibility_screen.*
 
 class AccessibilityFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         return inflater.inflate(
             R.layout.accessibility_screen,
             container, false
         )
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        kids_screen_toy.setOnClickListener {
-            it
-                .animate()
-                .rotation(
-                    kids_screen_toy.rotation + 180.0f
-                )
-        }
-    }
-
 }
